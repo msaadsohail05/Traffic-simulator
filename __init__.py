487e515653cea77182504f1c5f80b24a2539f78d
Muhammad Saad Sohail <msaad.bahria@gmail.com>
2026-03-27 16:45:08 +0500

    Change author name in __init__.py
    Updated author name in the package metadata.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ Smart Traffic Control Simulation Game
 """
 
 __version__ = "1.0.0"
-__author__ = "Traffic Simulator Team"
+__author__ = "Some Great Fastians"
 
 from src.entities import Car, Road, Intersection, TrafficLight, Direction, SignalState
 from src.simulation_engine import SimulationEngine
